64dce981e6e7578e88d54788be1b8628a6e4bc31
daodamain <fcfcqloows@gmail.com>
2018-01-06 02:36:18 +0900

    many

---
+++ temp.js
@@ -0,0 +1,7 @@
+$(function(){
+	$("#link").click(function(event) {
+		$("footer").html('<p id="link"> Link (。´･ω･)? </p> <a href="./kuzu/howTo_ver1.txt" download="howTo_ver1.txts">About library for java(ver1)&nbsp</a> <a href="./kuzu/howTo_ver2.txt" download="howTo_ver2.txt">About library for java(ver2)&nbsp</a><br> <a href="./kuzu/code1.txt" download="code_ver1.txt">Code for java(ver1)&nbsp</a> <a href="./kuzu/code2.1.txt" download="code_ver2.txt">Code for java(ver2)&nbsp</a><br> <a href="./gomibako/java/TwitterControll.java" download="TwitterControll.java">TwitterController for Twitter4j&nbsp</a> <br><br> <a href="https://developers.google.com/speed/libraries/">jquery by google</a><br> <a href="http://sass-lang.com/install">scss install</a><br> <a href="https://www.java.com/ja/">java download</a><br><br>');
+		
+	
+	});
+});
