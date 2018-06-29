d97b7d17da96d32482cc829ee213e6ec8135797a
daodamain <fcfcqloows@gmail.com>
2018-06-29 12:40:55 +0900

    new

--- temp.js
+++ temp.js
@@ -3,10 +3,12 @@ $(function(){
 	$("#link").click(function(event) {
 		$("footer").html('<p id="link"> Link (。´･ω･) </p> '+ 
 			'<a href="./kuzu/howTo_ver1.txt" download="howTo_ver1.txt">About library for java(ver1)　/　</a>' +
-			'<a href="./kuzu/howTo_ver2.txt" download="howTo_ver2.txt">About library for java(ver2)　/　</a><br>' + 
+			'<a href="./kuzu/howTo_ver2.txt" download="howTo_ver2.txt">About library for java(ver2)　/　</a>' + 
+			'<a href="./kuzu/howTo_ver5.txt" download="howTo_ver5.txt">About library for java(ver5)　/　</a><br>' + 
 			'<a href="./kuzu/code1.txt" download="code_ver1.txt">Code for java(ver1)　/　</a>' +
 			'<a href="./kuzu/code2.1.txt" download="code_ver2.txt">Code for java(ver2)　/　</a>' +
-			'<a href="./kuzu/code3.0.4.txt" download="code_ver3.0.4.txt">Code for java(ver3)　/　</a><br>' +
+			'<a href="./kuzu/code3.0.4.txt" download="code_ver3.0.4.txt">Code for java(ver3)　/　</a>' +
+			'<a href="./kuzu/code5.0.0.txt" download="code_ver5.0.0.txt">Code for java(ver5)　/　</a><br>' +
 			'<a href="./gomibako/java/TwitterControll.java" download="TwitterControll.java">TwitterController for Twitter4j&nbsp</a> <br><br>' +
 			'<a href="https://developers.google.com/speed/libraries/">jquery by google</a><br>' +
 			'<a href="http://sass-lang.com/install">scss install</a><br>' +
